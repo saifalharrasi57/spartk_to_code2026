84a3e8acf1e00f37c16f6ab3c91957292de00736
saif alharrasi <saifalharrasi57@gmail.com>
2026-07-01 09:22:46 +0400

    task one is done

---
+++ first task/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace first_task;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String name = "Sara";
+        int age = 21;
+        double height = 1.56;
+        Boolean Student = true;
+
+        Console.WriteLine($"Name: {name},Age: {age}, Height:{height}, Student: {Student}");
+    }
+}
