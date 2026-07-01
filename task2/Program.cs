7a436a4bb36074ecb606a1db87363f0b48b6a127
saif alharrasi <saifalharrasi57@gmail.com>
2026-07-01 09:59:06 +0400

    added task2

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("calculate the rectangle area and perimeter");
+        Console.Write("enter Rectangle legnth: ");
+        float length = float.Parse(Console.ReadLine());
+        Console.Write("enter rectangle width: ");
+        float Width = float.Parse(Console.ReadLine());
+        float area = length * Width;
+        float perimeter = 2 * (length + Width) ;
+        Console.WriteLine($"Reactangle Area is :{area}");
+        Console.WriteLine($"Perimeter is :{perimeter}");
+
+
+
+
+    }
+}
