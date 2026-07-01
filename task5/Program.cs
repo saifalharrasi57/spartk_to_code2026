e2adc60b0c3023cface830fc99021ec063b269b1
saif alharrasi <saifalharrasi57@gmail.com>
2026-07-01 10:56:55 +0400

    task 5 is done

---
+++ task5/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace task5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string state = " ";
+        Console.Write("enter a temperature in Celsius:");
+        float tempC = float.Parse(Console.ReadLine());
+        float f = ((tempC * 9 / 5) + 32) ;
+        if (tempC < 10)
+        {
+            state= "cold";
+        }
+        else if (tempC >= 10 && tempC <= 30)
+        {
+             state = "mid";
+        }
+        else if (tempC> 30)
+        {
+             state = "HOT";
+        }
+        Console.WriteLine($"temprature in Fahrenheit:{f}, classifies to be {state}");
+            
+
+
+
+    }
+}
